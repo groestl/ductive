2dd84b91aac73f2b4c114dc113d926d6eb56cf0f
Christian <christian@code.fm>
2016-11-21 22:51:05 +0100

    license

--- src/ductive/console/config/RegisterHealthChecksBean.java
+++ src/ductive/console/config/RegisterHealthChecksBean.java
@@ -1,3 +1,24 @@
+/*
+ 	Copyright (c) 2014 code.fm
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+ */
 package ductive.console.config;
 
 import java.util.Map;
@@ -14,21 +35,21 @@ import com.codahale.metrics.health.HealthCheck;
 import com.codahale.metrics.health.HealthCheckRegistry;
 
 public class RegisterHealthChecksBean {
-	
+
 	private static final Logger log = LoggerFactory.getLogger(RegisterHealthChecksBean.class);
-	
+
 	@Autowired(required=false) private Map<String,? extends HealthCheck> healthChecks;
 	@Autowired private HealthCheckRegistry healthCheckRegistry;
-	
+
 	@PostConstruct
 	public void init() {
 		if(MapUtils.isEmpty(healthChecks))
 			return;
-		
+
 		for(Entry<String,? extends HealthCheck> e : healthChecks.entrySet()) {
 			if(log.isDebugEnabled())
 				log.debug(String.format("registering health check '%s'",e.getKey()));
-			
+
 			healthCheckRegistry.register(e.getKey(),e.getValue());
 		}
 	}
